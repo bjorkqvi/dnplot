74ae4169c9d15b6806f64b93c533bdb154d985b5
Jan-Victor Björkqvist <janvb@met.no>
2024-06-27 13:49:41 +0200

    type hints and name changes

--- dnplot/plotters/plotters.py
+++ dnplot/plotters/plotters.py
@@ -1,19 +1,23 @@
 import matplotlib.pyplot as plt
 from cartopy import crs as ccrs
 from ..plot_functions import dnora_functions
+from typing import TYPE_CHECKING, Callable
+
+if TYPE_CHECKING:
+    from dnora.modelrun import ModelRun
 
 
 class Dnora:
-    def __init__(self, model):
+    def __init__(self, model: ModelRun):
         self.model = model
 
-    def grid(self, grid_plotter=dnora_functions.grid_plotter) -> None:
+    def grid(self, plotter: Callable = dnora_functions.grid_plotter) -> None:
         fig, ax = plt.subplots(1)
         fig_dict = {"fig": fig, "ax": ax}
-        fig_dict = grid_plotter(fig_dict, self.model)
+        fig_dict = plotter(fig_dict, self.model)
         fig_dict.get("fig").show()
 
-    def wind(self, wind_plotter=dnora_functions.wind_plotter):
+    def wind(self, plotter: Callable = dnora_functions.wind_plotter):
         fig, ax = plt.subplots(subplot_kw={"projection": ccrs.PlateCarree()})
         gl = ax.gridlines(
             crs=ccrs.PlateCarree(),
@@ -25,11 +29,11 @@ class Dnora:
         gl.top_labels = None
         gl.right_labels = None
         fig_dict = {"fig": fig, "ax": ax, "gl": gl}
-        fig_dict = wind_plotter(fig_dict, self.model)
+        fig_dict = plotter(fig_dict, self.model)
         fig_dict.get("fig").show()
 
-    def spectra(self, spectra_plotter=dnora_functions.spectra_plotter):
+    def spectra(self, plotter: Callable = dnora_functions.spectra_plotter):
         fig, ax = plt.subplots(subplot_kw={"polar": True})
         fig_dict = {"fig": fig, "ax": ax}
-        fig_dict = spectra_plotter(fig_dict, self.model)
+        fig_dict = plotter(fig_dict, self.model)
         fig_dict.get("fig").show()
