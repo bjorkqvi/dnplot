23b1732b22b3367950c15d6c24fc8a182f755dc6
Dominykas Jasas <dominykas.jasas@gmail.com>
2024-08-09 15:40:03 +0200

    simplified spectra plotter code

--- dnplot/plot_functions/plotly_functions.py
+++ dnplot/plot_functions/plotly_functions.py
@@ -99,29 +99,56 @@ def spectra1d_plotter(model: ModelRun):
         return fig
     app.run_server("0.0.0.0", 8000, debug=True)
 
+def xarray_to_dataframe(model) -> pd.DataFrame:
+    df=model.ds().to_dataframe()
+    df=df.reset_index()
+    col_drop=['lon','lat','inds']
+    df=df.drop(col_drop,axis='columns')
+    df.set_index('time', inplace=True)
+    df=df.resample('h').asfreq()
+    df=df.reset_index()
+    return df
+def calculate_correlation(x,y):
+    x_mean = x.mean()
+    y_mean = y.mean()
+    covariance = ((x - x_mean) * (y - y_mean)).mean()
+    x_var = ((x - x_mean) ** 2).mean()
+    y_var = ((y - y_mean) ** 2).mean()
+    x_std = x_var ** 0.5
+    y_std = y_var ** 0.5
+    correlation = covariance / (x_std * y_std)
+    
+    return correlation
+
+def calculate_RMSE(x,y):
+    X = x.values.reshape(-1,1)
+    linear=LinearRegression()
+    linear.fit(X,y)
+    a=linear.coef_[0]
+    b=linear.intercept_
+    y_estimated=(a*x+b)
+    y_rmse=(y-y_estimated)**2
+    RMSE=(y_rmse.mean())**0.5
+    return RMSE
+
+def linear_regression_line(x,y,fig):
+    X = x.values.reshape(-1,1)
+    linear=LinearRegression()
+    linear.fit(X,y)
+    x_range=np.linspace(0,np.ceil(X.max()),100)
+    y_range=linear.predict(x_range.reshape(-1,1))
+    fig.add_traces(go.Scatter(x=x_range.flatten(), y=y_range.flatten(), mode='lines', name='Linear regression',visible=True))
+    return fig
+
 #install numpy scikit-learn statsmodels
 from sklearn.linear_model import LinearRegression
 import plotly.graph_objects as go
 from scipy.stats import gaussian_kde
 def scatter_plotter(model: ModelRun, model1:ModelRun):
-    ds_model = model.waveseries()
+    ds_model=model.waveseries()
     ds1_model1=model1.waveseries()
-
-    df_model=ds_model.ds().to_dataframe()
-    df_model=df_model.reset_index()
-    col_drop=['lon','lat','inds']
-    df_model=df_model.drop(col_drop,axis='columns')
-    df1_model1=ds1_model1.ds().to_dataframe()
-    df1_model1=df1_model1.reset_index()
-    col_drop1=['lon','lat','inds']
-    df1_model1=df1_model1.drop(col_drop1, axis='columns')
-
-    df_model.set_index('time', inplace=True)
-    df_model=df_model.resample('h').asfreq()
-    df_model=df_model.reset_index()
-    df1_model1.set_index('time', inplace=True)
-    df1_model1=df1_model1.resample('h').asfreq()
-    df1_model1=df1_model1.reset_index()
+    df_model=xarray_to_dataframe(model.waveseries())
+    df1_model1=xarray_to_dataframe(model1.waveseries())
 
     common_columns = list(set(df_model.columns).intersection(set(df1_model1.columns)))
     df = pd.merge(df_model[common_columns], df1_model1[common_columns], on='time', suffixes=(f' {ds_model.name}',  f' {ds1_model1.name}'))
@@ -129,6 +156,8 @@ def scatter_plotter(model: ModelRun, model1:ModelRun):
     df.insert(0,'time',first_column)
     df_column=[col for col in df.columns if col.endswith(f' {ds_model.name}')]
     df1_column=[col for col in df.columns if col.endswith(f' {ds1_model1.name}')]
+    df_noNa=df.dropna().reset_index(drop=True)
+    breakpoint()
     app = Dash(__name__)
     app.layout = html.Div([
         html.H1(ds_model.name, style={'textAlign': 'center'}),
@@ -158,106 +187,65 @@ def scatter_plotter(model: ModelRun, model1:ModelRun):
     def update_graph(x_var,y_var):
         x_col=f'{x_var}'
         y_col=f'{y_var}'
-        df_noNa=df.dropna().reset_index(drop=True)
         """
         Calculates the correlation
         """
-        x_mean=df_noNa[x_col].mean()
-        y_mean=df_noNa[y_col].mean()
-        cov_sum=0
-        n=df_noNa[x_col]
-        for i in range(len(n)):
-            x_dev=df_noNa[x_col].iloc[i]-x_mean
-            y_dev=df_noNa[y_col].iloc[i]-y_mean
-            cov_sum=cov_sum + (x_dev*y_dev)
-        covariance = cov_sum / len(n)
-        x_varsum=0
-        y_varsum=0
-        for i in range(len(n)):
-            x=(df_noNa[x_col].iloc[i]-x_mean)**2
-            y=(df_noNa[y_col].iloc[i]-y_mean)**2
-            x_varsum=x_varsum + x
-            y_varsum=y_varsum + y
-        x_var = x_varsum/len(n)
-        y_var = y_varsum/len(n)
-        x_std = (x_var)**0.5
-        y_std = (y_var)**0.5
-        correlation = covariance/(x_std*y_std)
+        correlation=calculate_correlation(df_noNa[x_col],df_noNa[y_col])
         """
         Calculates RMSE
         Calculates SI
         """
-        if x_col not in df.columns or y_col not in df.columns:
-            return go.Figure()
-        X = df_noNa[[x_col]].values.reshape(-1,1)
-        model=LinearRegression()
-        model.fit(X,df_noNa[[y_col]])
-        a=model.coef_[0][0]
-        b=model.intercept_[0]
-        y_estimated=(a*df_noNa[x_col]+b)
-        y_rmse=0
-        for i in range(len(n)):
-            y=(df_noNa[y_col].iloc[i]-y_estimated.iloc[i])**2
-            y_rmse=y_rmse + y
-        RMSE=(y_rmse/len(n))**0.5
-
-        SI=RMSE/x_mean
-
+        RMSE=calculate_RMSE(df_noNa[x_col],df_noNa[y_col])
+        SI=RMSE/df_noNa[x_col].mean()
+        """
+        Stack values and
+        Calculates density.
+        """
         xy = np.vstack([df_noNa[x_col].values, df_noNa[y_col].values])
         z = gaussian_kde(xy)(xy)
 
-        x_range=np.linspace(0,np.ceil(X.max()),100)
-        y_range=model.predict(x_range.reshape(-1,1))
+        if x_col not in df.columns or y_col not in df.columns:
+            return go.Figure()
         fig = px.scatter(df_noNa, x=x_col, y=y_col,color=z, color_continuous_scale='jet')
+
+        linear_regression_line(df_noNa[x_col],df_noNa[y_col],fig)
 
         x_max=np.ceil(df_noNa[x_col].max())
         y_max=np.ceil(df_noNa[y_col].max())
-        
-        fig.add_traces(go.Scatter(x=x_range.flatten(), y=y_range.flatten(), mode='lines', name='Linear regression',visible=True))
-        x_values = np.linspace(0, np.ceil(df_noNa[x_col].max()), 100)
+
+        x_values = np.linspace(0, np.ceil(x_max), 100)
         y_values = x_values
         fig.add_traces(go.Scatter(x=x_values, y=y_values, mode='lines', name='x=y',visible='legendonly'))
-        x_line=np.linspace(0,np.ceil(df_noNa[x_col].max()), 100)
+
+        x_line=np.linspace(0,np.ceil(x_max), 100)
         a=np.sum(df_noNa[x_col]*df_noNa[y_col])/np.sum(df_noNa[x_col]**2)
         y=a*x_line
         fig.add_traces(go.Scatter(x=x_line, y=y, mode='lines', name='one-parameter-linear regression',visible='legendonly'))
 
-
         if x_max > y_max:
             fig.update_layout(
-                yaxis=dict(
-                    range=[0, x_max]
-                ),
-                xaxis=dict(
-                    range=[0, x_max]
-                )
+                yaxis=dict(range=[0, x_max]),
+                xaxis=dict(range=[0, x_max])
             )
         else: 
             fig.update_layout(
-                xaxis=dict(
-                    range=[0, y_max]
-                ),
-                yaxis=dict(
-                    range=[0, y_max]
-                )
+                xaxis=dict(range=[0, y_max]),
+                yaxis=dict(range=[0, y_max])
             )
-
         fig.update_layout(
             coloraxis_colorbar=dict(
                 title='Density',
-                y=0.45,
-                x=1.015,
+                y=0.45, x=1.015,
                 len=0.9
             ),
             annotations=[
                 dict(
-                    x=0.001,
-                    y=0.995,
+                    x=0.001,y=0.995,
                     xref='paper',
                     yref='paper',
                     text=(
                         f'N = {len(df[x_col])}<br>'
-                        f'Bias = {x_mean - y_mean:.4f}<br>'
+                        f'Bias = {df_noNa[x_col].mean() - df_noNa[y_col].mean():.4f}<br>'
                         f'R\u00b2= {correlation:.4f}<br>'
                         f'RMSE= {RMSE:.4F}<br>'
                         f'SI= {SI:.4F}'
@@ -276,10 +264,7 @@ def scatter_plotter(model: ModelRun, model1:ModelRun):
             width=1800,
             height=900,
             margin=dict(
-                l=0,
-                r=0,   
-                t=40,     
-                b=0
+                l=0,r=0,t=40,b=0
             )
         )
         return fig
