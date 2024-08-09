13b70959b4febde294e635403ce91c0f6b7e8258
Dominykas Jasas <dominykas.jasas@gmail.com>
2024-08-09 11:14:43 +0200

    Made waveseries.df, updated layout of scatter plot

--- dnplot/plot_functions/plotly_functions.py
+++ dnplot/plot_functions/plotly_functions.py
@@ -21,7 +21,7 @@ def waveseries_plotter(model: ModelRun):
     variables = [col for col in var.columns if col != 'time']
 
     for variable in variables:
-        trace = go.Scatter(x=var['time'], y=var[variable], mode='lines', name=variable)
+        trace = go.Scatter(x=var['time'], y=var[variable], mode='lines', name=variable, visible='legendonly')
         fig.add_trace(trace)
 
     fig.update_layout(
@@ -104,59 +104,46 @@ from sklearn.linear_model import LinearRegression
 import plotly.graph_objects as go
 from scipy.stats import gaussian_kde
 def scatter_plotter(model: ModelRun, model1:ModelRun):
-    e39 = model.waveseries()
-    nora3=model1.waveseries()
-    e39_ds={
-        'time':e39.time(),
-        'hs': e39.hs(),
-        'hs_max': e39.hs_max(),
-        'tm01': e39.tm01(),
-        'tm02': e39.tm02(),
-        'tp': e39.tp(),
-        'tz': e39.tz(),
-        'dirm_sea': e39.dirm_sea(),
-        'dirs': e39.dirs(),
-    }
-    nora3_ds={
-        'time':nora3.time(),
-        'hs':nora3.hs(),
-        'tm01':nora3.tm01(),
-        'tm02':nora3.tm02(),
-        'tm_10':nora3.tm_10(),
-        'dirm':nora3.dirm(),
-    }
+    ds_model = model.waveseries()
+    ds1_model1=model1.waveseries()
 
-    e39_df=pd.DataFrame(e39_ds)
-    nora3_df=pd.DataFrame(nora3_ds)
-    e39_df.set_index('time', inplace=True)
-    e39_df=e39_df.resample('h').asfreq()
-    e39_df=e39_df.reset_index()
-    common_columns = list(set(e39_df.columns).intersection(set(nora3_df.columns)))
-    df = pd.merge(e39_df[common_columns], nora3_df[common_columns], on='time', suffixes=('_e39','_nora3'))
+    df_model=ds_model.ds().to_dataframe()
+    df_model=df_model.reset_index()
+    col_drop=['lon','lat','inds']
+    df_model=df_model.drop(col_drop,axis='columns')
+    df1_model1=ds1_model1.ds().to_dataframe()
+    df1_model1=df1_model1.reset_index()
+    col_drop1=['lon','lat','inds']
+    df1_model1=df1_model1.drop(col_drop1, axis='columns')
+
+    df_model.set_index('time', inplace=True)
+    df_model=df_model.resample('h').asfreq()
+    df_model=df_model.reset_index()
+    df1_model1.set_index('time', inplace=True)
+    df1_model1=df1_model1.resample('h').asfreq()
+    df1_model1=df1_model1.reset_index()
+
+    common_columns = list(set(df_model.columns).intersection(set(df1_model1.columns)))
+    df = pd.merge(df_model[common_columns], df1_model1[common_columns], on='time', suffixes=(f' {ds_model.name}',  f' {ds1_model1.name}'))
     first_column=df.pop('time')
     df.insert(0,'time',first_column)
+    df_column=[col for col in df.columns if col.endswith(f' {ds_model.name}')]
+    df1_column=[col for col in df.columns if col.endswith(f' {ds1_model1.name}')]
     app = Dash(__name__)
     app.layout = html.Div([
-        html.H1(nora3.name, style={'textAlign': 'center'}),
+        html.H1(ds_model.name, style={'textAlign': 'center'}),
+        html.P("Select variable:"),
         dcc.Dropdown(
             id="x-axis-dropdown",
-            options=[
-                {"label": "hs", "value": "hs"},
-                {"label": "tm01", "value": "tm01"},
-                {"label": "tm02", "value": "tm02"},
-            ],
-            value="hs",
+            options = [{'label': col, 'value': col} for col in df_column],
+            value=f'hs {ds_model.name}',
             clearable=False,
             style={'width': '30%'}, 
         ),
         dcc.Dropdown(
             id="y-axis-dropdown",
-            options=[
-                {"label": "hs", "value": "hs"},
-                {"label": "tm01", "value": "tm01"},
-                {"label": "tm02", "value": "tm02"},
-            ],
-            value="hs",
+            options=[{'label': col, 'value': col} for col in df1_column],
+            value=f'hs {ds1_model1.name}',
             clearable=False,
             style={'width': '30%'},
         ),
@@ -169,8 +156,8 @@ def scatter_plotter(model: ModelRun, model1:ModelRun):
         Input("y-axis-dropdown", "value"), 
     )
     def update_graph(x_var,y_var):
-        x_col=f'{x_var}_e39'
-        y_col=f'{y_var}_nora3'
+        x_col=f'{x_var}'
+        y_col=f'{y_var}'
         df_noNa=df.dropna().reset_index(drop=True)
         """
         Calculates the correlation
@@ -199,7 +186,6 @@ def scatter_plotter(model: ModelRun, model1:ModelRun):
         """
         Calculates RMSE
         Calculates SI
-        Makes a linear regression line
         """
         if x_col not in df.columns or y_col not in df.columns:
             return go.Figure()
@@ -219,15 +205,49 @@ def scatter_plotter(model: ModelRun, model1:ModelRun):
 
         xy = np.vstack([df_noNa[x_col].values, df_noNa[y_col].values])
         z = gaussian_kde(xy)(xy)
-        x_range=np.linspace(X.min(),X.max(),100)
+
+        x_range=np.linspace(0,np.ceil(X.max()),100)
         y_range=model.predict(x_range.reshape(-1,1))
         fig = px.scatter(df_noNa, x=x_col, y=y_col,color=z, color_continuous_scale='jet')
-        fig.add_traces(go.Scatter(x=x_range.flatten(), y=y_range.flatten(), name='Linear regression'))
+
+        x_max=np.ceil(df_noNa[x_col].max())
+        y_max=np.ceil(df_noNa[y_col].max())
+        
+        fig.add_traces(go.Scatter(x=x_range.flatten(), y=y_range.flatten(), mode='lines', name='Linear regression',visible=True))
+        x_values = np.linspace(0, np.ceil(df_noNa[x_col].max()), 100)
+        y_values = x_values
+        fig.add_traces(go.Scatter(x=x_values, y=y_values, mode='lines', name='x=y',visible='legendonly'))
+        x_line=np.linspace(0,np.ceil(df_noNa[x_col].max()), 100)
+        a=np.sum(df_noNa[x_col]*df_noNa[y_col])/np.sum(df_noNa[x_col]**2)
+        y=a*x_line
+        fig.add_traces(go.Scatter(x=x_line, y=y, mode='lines', name='one-parameter-linear regression',visible='legendonly'))
+
+
+        if x_max > y_max:
+            fig.update_layout(
+                yaxis=dict(
+                    range=[0, x_max]
+                ),
+                xaxis=dict(
+                    range=[0, x_max]
+                )
+            )
+        else: 
+            fig.update_layout(
+                xaxis=dict(
+                    range=[0, y_max]
+                ),
+                yaxis=dict(
+                    range=[0, y_max]
+                )
+            )
+
         fig.update_layout(
             coloraxis_colorbar=dict(
                 title='Density',
                 y=0.45,
                 x=1.015,
+                len=0.9
             ),
             annotations=[
                 dict(
@@ -251,6 +271,16 @@ def scatter_plotter(model: ModelRun, model1:ModelRun):
                     opacity=0.55
                 )
             ]
+        )
+        fig.update_layout(
+            width=1800,
+            height=900,
+            margin=dict(
+                l=0,
+                r=0,   
+                t=40,     
+                b=0
+            )
         )
         return fig
     app.run_server("0.0.0.0", 8000, debug=True)
