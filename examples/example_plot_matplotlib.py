f947b4b8c9c9303e99e8d2f69fd231d610ce254d
Dominykas Jasas <dominykas.jasas@gmail.com>
2024-08-19 12:05:58 +0200

    Organized examples for Plotly and Matplotlib in separate files

--- examples/example_plot_matplotlib.py
+++ examples/example_plot_matplotlib.py
@@ -6,25 +6,25 @@ import dnplot
 # os.environ["DNORA_LOCAL_GRID_PATH"] = "~/bathy"
 
 """
-These examples plots are using library plotly
+This examples are using matplotlib library to plot examples
 """
 
-#Plots a barpolar spectra plot together with spectra1D a map with location points.  
-#Plots separete spectra1d plot.
+#plots a spectra polar (plot.spectra())
+#plots a spectra 1D (plot.spectra1d())
 grid = dn.grid.EMODNET(lon=(4, 6), lat=(59, 60))
 grid.import_topo(folder="~/kodai/bathy")
 grid.set_spacing(dm=5000)
 grid.mesh_grid()
 model = dn.modelrun.NORA3(grid, year=2022, month=2, day=1)
+model.import_wind()
 model.import_spectra()
 model.spectra_to_1d()
 
-plot = dnplot.Plotly(model)
+plot = dnplot.Dnora(model)
 plot.spectra()
 plot.spectra1d()
 
-
-#Plots a scatter plot 
+#plots a scatter plot 
 e39 = dn.modelrun.ModelRun(year=2019, month=3)
 e39.import_waveseries(dn.waveseries.read.E39(loc="D"), point_picker=dn.pick.Trivial())
 
@@ -32,15 +32,15 @@ point = dn.grid.Grid(lon=e39.waveseries().lon(), lat=e39.waveseries().lat())
 nora3 = dn.modelrun.NORA3(point, year=2019, month=3)
 nora3.import_spectra()
 nora3.spectra_to_waveseries()
-plot = dnplot.Plotly1(e39, nora3)
+plot = dnplot.Dnora(model)
 plot.scatter()
 
-#Plots waveseries plot
+#plots a waveseries plot
 point = dn.grid.Grid(lon=4.308, lat=62.838, name="Svinoy")
 model = dn.modelrun.NORA3(point, year=2022, month=3, day=18)
 model.import_spectra()
 model.spectra_to_waveseries()
 model.waveseries()
-plot = dnplot.Plotly(model)
-plot.waveseries(use_dash=True)
+plot=dnplot.Dnora(model)
+plot.waveseries()
 
